b064e672e87aec77dfaf9f90ab7567e6079adc72
levie <levie@DESKTOP-4CCO1AI.cpe.cableonda.net>
2017-12-21 16:32:53 +0200

    try

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,13 @@ import db.wifiDB;
 import files.exportCsv;
 import files.exportKML;
 import files.importCsv;
+import filters.locationFilter;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException, ParseException {
 		// TODO Auto-generated method stub
-		
+
 		scanDB DB = importCsv.csvMergeTodb("C:/1/r", 1);
 		wifiDB wDB = runAlgo.Algo1(DB);
 		exportCsv.wifiDBtoCsv("C:/1/e/test2.csv", wDB);
@@ -23,7 +24,8 @@ public class Main {
 		exportCsv.DBtoCsv("C:/1/e/test.csv", DBnoGps);
 		exportKML.dbToKml("C:/1/e/test1.kml", DB);
 		exportKML.dbToKml("C:/1/e/test2.kml", DBnoGps);
-	
+
+		
 		
 		
 		
